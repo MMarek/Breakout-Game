0093912abebd5fcba85e52939a96e791384631f5
MMarek <mildnermarek@gmail.com>
2020-01-03 15:55:13 +0100

    dzwiek on/off, wygrana zwyciestwo, przegrana, restart

--- game.js
+++ game.js
@@ -69,8 +69,8 @@ const ball = {
     x: cvs.width / 2,
     y: paddle.y - ball_radius,
     radius: ball_radius,
-    speed : 4,
-    dx: 3 * (Math.random() * 2-1),
+    speed: 4,
+    dx: 3 * (Math.random() * 2 - 1),
     dy: -3
 };
 
@@ -104,64 +104,68 @@ function ballWallCollision() {
         ball.dy = -ball.dy;
         wall_hit.play();
     }
-    if(ball.y + ball.radius > cvs.height){
-        LIFE -- ; //utrata życia
+    if (ball.y + ball.radius > cvs.height) {
+        LIFE--; //utrata życia
         life_lost.play()
         resetBall();
     }
 }
+
 // funkcja restartu piłeczki
 function resetBall() {
-    ball.x = cvs.width/2;
+    ball.x = cvs.width / 2;
     ball.y = paddle.y - ball.radius;
-    ball.dx = 3 * (Math.random() * 2-1);
+    ball.dx = 3 * (Math.random() * 2 - 1);
     ball.dy = -3
 }
+
 // funkcja kolizji piłeczki i paletki
 function ballPaddleCollision() {
-    if(ball.x < paddle.x + paddle.width
+    if (ball.x < paddle.x + paddle.width
         && ball.x > paddle.x
         && paddle.y < paddle.y + paddle.height
-        && ball.y > paddle.y){
+        && ball.y > paddle.y) {
 
         // dzwiek
         paddle_hit.play();
 
         // sprawdz gdzie piłeczka uderzy paletke
-        let collidePoint = ball.x - (paddle.x + paddle.width/2);
+        let collidePoint = ball.x - (paddle.x + paddle.width / 2);
 
         //normalizacja wartości
-        collidePoint = collidePoint / (paddle.width/2);
+        collidePoint = collidePoint / (paddle.width / 2);
 
         // przeliczenie kąta piłeczki
-        let angle = collidePoint * Math.PI/3;
+        let angle = collidePoint * Math.PI / 3;
 
         ball.dx = ball.speed * Math.sin(angle);
-        ball.dy = - ball.speed * Math.cos(angle);
+        ball.dy = -ball.speed * Math.cos(angle);
     }
 }
+
 // tworzenie klocków
 const brick = {
-    row : 1,
-    column : 5,
-    width : 55,
-    height : 20,
-    offsetLeft : 20,
-    offsetTop : 20,
-    marginTop : 40,
-    fillColor : "black",
-    strokeColor : "#FFF"
+    row: 1,
+    column: 5,
+    width: 55,
+    height: 20,
+    offsetLeft: 20,
+    offsetTop: 20,
+    marginTop: 40,
+    fillColor: "black",
+    strokeColor: "#FFF"
 };
 
 let bricks = [];
+
 function createBricks() {
     for (let r = 0; r < brick.row; r++) {
         bricks[r] = [];
         for (let c = 0; c < brick.column; c++) {
             bricks[r][c] = {
-                x : c * (brick.offsetLeft + brick.width) + brick.offsetLeft,
-                y : r * (brick.offsetTop + brick.height) + brick.offsetTop + brick.marginTop,
-                status : true
+                x: c * (brick.offsetLeft + brick.width) + brick.offsetLeft,
+                y: r * (brick.offsetTop + brick.height) + brick.offsetTop + brick.marginTop,
+                status: true
             }
         }
     }
@@ -174,8 +178,8 @@ function drawBricks() {
     for (let r = 0; r < brick.row; r++) {
         for (let c = 0; c < brick.column; c++) {
             let b = bricks[r][c];
-                // jezeli klocek nie jest uszkodzony
-                if(b.status){
+            // jezeli klocek nie jest uszkodzony
+            if (b.status) {
                 ctx.fillStyle = brick.fillColor;
                 ctx.fillRect(b.x, b.y, brick.width, brick.height);
 
@@ -194,7 +198,7 @@ function ballBrickCollision() {
             // jezeli klocek nie jest uszkodzony
             if (b.status) {
                 if (ball.x + ball.radius > b.x && ball.x - ball.radius < b.x + brick.width &&
-                ball.y + ball.radius > b.y && ball.y - ball.radius < b.y + brick.height){
+                    ball.y + ball.radius > b.y && ball.y - ball.radius < b.y + brick.height) {
                     brick_hit.play();
                     ball.dy = -ball.dy;
                     b.status = false; //gdy klocek zostanie uszkodzony
@@ -226,17 +230,18 @@ function draw() {
     // pokaż punktacje
     showGameStats(score, 35, 25, score_img, 5, 5);
     // pokaż życia
-    showGameStats(LIFE, cvs.width - 25, 25, life_img, cvs.width-55, 5);
+    showGameStats(LIFE, cvs.width - 25, 25, life_img, cvs.width - 55, 5);
     // pokaż poziom
-    showGameStats(level, cvs.width/2, 25, level_img, cvs.width/2-30, 5);
+    showGameStats(level, cvs.width / 2, 25, level_img, cvs.width / 2 - 30, 5);
 }
 
 // koniec gry
 function gameOver() {
-    if(LIFE <=0){
+    if (LIFE <= 0) {
         game_over = true;
     }
 }
+
 // poziom wyżej
 function levelUp() {
     let isLevelDone = true;
@@ -244,13 +249,13 @@ function levelUp() {
     // sprawdz czy wszystkie klocki są zbite/uszkodzone
     for (let r = 0; r < brick.row; r++) {
         for (let c = 0; c < brick.column; c++) {
-           isLevelDone = isLevelDone && ! bricks[r][c].status;
+            isLevelDone = isLevelDone && !bricks[r][c].status;
         }
     }
 
-    if(isLevelDone){
-        if(level >= max_level){
-            win.play();
+    if (isLevelDone) {
+        win.play();
+        if (level >= max_level) {
             game_over = true;
             return;
         }
@@ -279,9 +284,29 @@ function loop() {
     draw();
     update();
 
-    if(! game_over){
+    if (!game_over) {
         webkitRequestAnimationFrame(loop);
     }
 }
 
 loop();
+
+// wybierz elem. dzwięku
+const soundElement = document.getElementById("sound");
+
+soundElement.addEventListener("click", audioManager);
+
+function audioManager() {
+// zmień img dzwięku z ON na OFF
+    let imgSrc = soundElement.getAttribute("src");
+    let SOUND_ON = imgSrc == "img/SOUND_ON.png" ? "img/SOUND_OFF.png" : "img/SOUND_ON.png";
+
+    soundElement.setAttribute("src", SOUND_IMG); //??
+
+// załącz i wyłącz dzwiek
+    wall_hit.muted = wall_hit.muted ? false : true;
+    paddle_hit.muted = paddle_hit.muted ? false : true;
+    brick_hit.muted = brick_hit.muted ? false : true;
+    win.muted = win.muted ? false : true;
+    life_lost.muted = life_lost.muted ? false : true;
+}
